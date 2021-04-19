41d1381f216e9dff11a06cbb7fd42c10910790f6
joonsunglee <john121637@gmail.com>
2021-04-19 23:51:17 +0900

    add product list page

---
+++ client/productlistpage.js
@@ -0,0 +1,18 @@
+var items = ["one", "two", "three", "four", "five", "six"]
+
+var currentindex = 0
+
+function loadmore(){
+    var maxresult = 1
+    for(var i = 0; i < maxresult; i++){
+        if(currentindex >= items.length){
+            $("#lmbutton").hide()
+            return
+        }
+        // $("#content").append("<div>"+items[i+currentindex]+"</div>")
+        $("#content").append('<div class="img"> <div class="img_arrange"> <a href="#"><img src="#" width="300" height="300"></a> </div> <div class="img_arrange"> <a href="#"><img src="#" width="300" height="300"></a> </div> <div class="img_arrange"> <a href="#"><img src="#" width="300" height="300"></a> </div> </div>')
+    }
+    currentindex += maxresult
+}
+
+loadmore()
